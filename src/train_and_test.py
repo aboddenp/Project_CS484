297cd69e7159449f232d3ed7c1e220d9c477b5d4
boddenme <43423531+aboddenp@users.noreply.github.com>
2019-04-29 10:21:00 -0400

    encoding labels to numbers

--- src/train_and_test.py
+++ src/train_and_test.py
@@ -1,6 +1,7 @@
 #authors Aster Bodden and Naassom Rocha 
 # This source code is responsible for splitting the data into Train and Test and its corresponding ground truth 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 import pandas 
 
 # remove the instances that do not have a label 
@@ -8,12 +9,17 @@ raw_data = pandas.read_csv("wiki_movie_plots_deduped.csv")
 
 clean_data = raw_data[raw_data.Genre != "unknown"]
 
+encoder = LabelEncoder()
+encoder.fit(list(clean_data['Genre'].values))
+
 # split data into train and test data 
 # set the train_size to the percent of data to use as training 
 train, test = train_test_split(clean_data, train_size = 0.7, random_state = 42)
-labels = test.Genre 
+labels = pandas.DataFrame(test.Genre, columns = ["id","Genre"] ) 
 test = test.drop("Genre", axis = 1)
-# if w
+ 
+train['encodedLabel'] = encoder.transform(list(train['Genre'].values))
+labels['encodedLabel'] = encoder.transform(list(labels['Genre'].values))
 
 # write the csv in its corresponding file 
 train.to_csv("train.csv")

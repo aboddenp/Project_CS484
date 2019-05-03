55c1717b72ef91323f54dfae11fb9e6d32dea84f
boddenme <43423531+aboddenp@users.noreply.github.com>
2019-05-02 22:26:42 -0400

    refactor

--- src/MultiLabelClassifier.py
+++ src/MultiLabelClassifier.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
+# classifiers 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+#######################################################
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer
@@ -9,11 +12,13 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics import hamming_loss  # more forgiving metric its special for multilabel or multiclass problems 
 from sklearn.metrics import accuracy_score
+
 import re
 
 
 # global fields 
 stemmer = PorterStemmer()
+multi_binarizer = MultiLabelBinarizer(sparse_output= False)
 
 
 #Functions: 
@@ -27,42 +32,101 @@ def tokenize(plot):
     return plot
 
 
+# returns the binarized labels for both the train and test data 
+def binarizeLabels(train_labels, test_lables ): 
+	train_labels = multi_binarizer.fit_transform(train_labels)
+	test_lables = multi_binarizer.transform(test_lables)
+	return train_labels, test_lables
+
+# given the data and the binarized multi labels this  function returns the predictions from a knn
+# if data is documents then please use tfid = True ti make a document table 
+def knn(data,labels,test, k = 1 , met = "cosine", wei = "distance", tfid = False, reduct = True):
+	if(tfid):
+		# # make vector document with inverse frequency 
+		vec = TfidfVectorizer(stop_words="english") # add tokenizer = tokenize makes the vectorizer too slow 
+		data = vec.fit_transform(data)
+		test = vec.transform(test)
+		if(reduct):
+			data, test = dimReduction(data,test, n = 100)
+
+	# classify 
+	KNNclf = KNeighborsClassifier(n_neighbors= k , metric= met , weights = wei)
+	KNNclf.fit(data, labels)
+	predictions = KNNclf.predict(test)
+	return predictions
+
+def dTree(data, labels, test, inpurity = "Gini", mdepth = None): 
+	 tree = DecisionTreeClassifier(max_depth = mdepth , random_state=42 )
+	 tree.fit(data,labels)
+	 return tree.predict(test)
+
+
+# returns reduced data given n as number of components and method as the reduction algorithm 
+def dimReduction(train, test, n = 100,  method = "tsvd"): 
+	# feature selection
+	if(method == "tsvd"): 
+		pca = TruncatedSVD(n_components = n, random_state=42)
+		train = pca.fit_transform(train)
+		test = pca.transform(test)
+		return train, test 
+
+# given the ground truth and the predictions this will return three metrics for evaluation 
+def  evaluate(ground_truth, predictions): 
+	score = f1_score(ground_truth,predictions, average = 'weighted')
+	hamm = hamming_loss(ground_truth,predictions)
+	accuracy = accuracy_score(ground_truth,predictions)
+	return score, hamm ,accuracy
+
+
 df = pd.read_csv("train.csv", header=0)
+df["Cast"] = df["Cast"].fillna("None")
 df["GenreCorrected"] = df["GenreCorrected"].str.split('|')
 train, validation = train_test_split(df, train_size = 0.7, random_state = 42)
+
 
 genres = train["GenreCorrected"]
 validation_genres = validation["GenreCorrected"]
 
-multi_binarizer = MultiLabelBinarizer(sparse_output= False)
-labels = multi_binarizer.fit_transform(genres)
-validation_labels = multi_binarizer.transform(validation_genres)
+genres, validation_genres = binarizeLabels(genres,validation_genres)
 
-plot_train = train["Plot"]
-plot_validation = validation["Plot"]
+print(train.shape)
 
-# make vector document with inverse frequency 
-vec = TfidfVectorizer(stop_words="english") # add tokenizer = tokenize makes the vectorizer too slow 
-train_plot = vec.fit_transform(plot_train)
-validation_plot = vec.transform(plot_validation)
+# requieres converting the values into numerical 
+#predictions = dTree(train[["Release Year", "Title", "Origin/Ethnicity","Director"]], genres, validation[["Release Year", "Title", "Origin/Ethnicity", "Director"] ])
+#print(evaluate(predictions, validation_genres))
+predictions = knn(train["Cast"],genres,validation["Cast"],k = 3, tfid = True)
+print(evaluate(predictions, validation_genres))
+predictions = knn(train["Plot"],genres,validation["Plot"],k = 3, tfid = True)
+print(evaluate(predictions, validation_genres))
 
-# feature selection 
-pca = TruncatedSVD(n_components = 100, random_state=42)
-train_plot = pca.fit_transform(train_plot)
-validation_plot = pca.transform(validation_plot)
+# multi_binarizer = MultiLabelBinarizer(sparse_output= False)
+# labels = multi_binarizer.fit_transform(genres)
+# validation_labels = multi_binarizer.transform(validation_genres)
 
-# classify 
-KNNclf = KNeighborsClassifier(n_neighbors=1, metric= "cosine", weights = "distance")
-KNNclf.fit(train_plot, labels)
-predictions = KNNclf.predict(validation_plot)
+# plot_train = train["Plot"]
+# plot_validation = validation["Plot"]
+
+# # make vector document with inverse frequency 
+# vec = TfidfVectorizer(stop_words="english") # add tokenizer = tokenize makes the vectorizer too slow 
+# train_plot = vec.fit_transform(plot_train)
+# validation_plot = vec.transform(plot_validation)
+
+# # feature selection 
+# pca = TruncatedSVD(n_components = 100, random_state=42)
+# train_plot = pca.fit_transform(train_plot)
+# validation_plot = pca.transform(validation_plot)
+
+# # classify 
+# KNNclf = KNeighborsClassifier(n_neighbors=1, metric= "cosine", weights = "distance")
+# KNNclf.fit(train_plot, labels)
+# predictions = KNNclf.predict(validation_plot)
  
-# print the f1 score 
+# # print the f1 score 
+# score = f1_score(validation_labels,predictions, average = 'weighted')
+# hamm = hamming_loss(validation_labels,predictions)
+# accuracy = accuracy_score(validation_labels,predictions)
+# print(score)
+# print(str(hamm) + " hamming loss ")
+# print(str(accuracy))
 
-score = f1_score(validation_labels,predictions, average = 'weighted')
-hamm = hamming_loss(validation_labels,predictions)
-accuracy = accuracy_score(validation_labels,predictions)
-print(score)
-print(str(hamm) + " hamming loss ")
-print(str(accuracy))
-
-predictions = multi_binarizer.inverse_transform(predictions)
+# predictions = multi_binarizer.inverse_transform(predictions)
